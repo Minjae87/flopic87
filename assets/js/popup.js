dad2b94b0b8f1eb44764f355131a7d40afbb7dd4
Minjae87 <kimalswo@naver.com>
2022-03-23 10:32:46 +0900

    Add files via upload

---
+++ assets/js/popup.js
@@ -0,0 +1,58 @@
+
+
+$(function () {
+
+	$("#button1").click(function () {//.button1은 눌렀을때 팝업을 뜨게 할 대상!
+		$(".popup1").fadeIn();
+
+	});
+	$(".close").click(function () {
+		$(".popup1").fadeOut();
+
+	});
+
+
+	$("#button2").click(function () {//.button2은 눌렀을때 팝업을 뜨게 할 대상!
+		$(".popup2").fadeIn();
+
+	});
+	$(".close").click(function () {
+		$(".popup2").fadeOut();
+
+	});
+
+
+	$("#button3").click(function () {//.button3은 눌렀을때 팝업을 뜨게 할 대상!
+		$(".popup3").fadeIn();
+
+	});
+	$(".close").click(function () {
+		$(".popup3").fadeOut();
+
+	});
+
+
+	$("#button4").click(function () {//.button4은 눌렀을때 팝업을 뜨게 할 대상!
+		$(".popup4").fadeIn();
+
+	});
+	$(".close").click(function () {
+		$(".popup4").fadeOut();
+
+	});
+
+	
+	$("#button5").click(function () {//.button5은 눌렀을때 팝업을 뜨게 할 대상!
+		$(".popup5").fadeIn();
+
+	});
+	$(".close").click(function () {
+		$(".popup5").fadeOut();
+
+	});
+
+
+
+
+
+});
